bcb9824a75f6a52a804612f4e950964630a5475c
Jacob <jacobgsdev@gmail.com>
2025-01-02 17:20:22 +0100

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SteamWishlistApp {
+    public partial class App : Application {
+        public App() {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
