4c28f587b523b1d50f92a11fcba847fba650ed96
Jacob <jacobgsdev@gmail.com>
2025-01-02 17:58:01 +0100

    Created the right kind of class library

--- SteamStoreAPI/Class1.cs
+++ SteamStoreAPI/Class1.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace SteamStoreAPI {
+    public class Class1 {
 
-namespace SteamStoreAPI
-{
-    public class Class1
-    {
     }
 }
