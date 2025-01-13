f1e7515846a6b2eed667590087db91b43c207d68
Jacob <jacobgsdev@gmail.com>
2025-01-13 18:28:07 +0100

    Updated properties

--- SteamWishlistApp/Views/FriendPage.xaml.cs
+++ SteamWishlistApp/Views/FriendPage.xaml.cs
@@ -18,7 +18,9 @@ public partial class FriendPage : ContentPage
 
     private async void AddGame_Clicked(object sender, EventArgs e) {
         var app = await MauiProgram.DataClient.GetSteamApp(1245620); // Elden ring
+        var app2 = await MauiProgram.DataClient.GetSteamApp(516750); // My summer car
         ((FriendViewModel)BindingContext).Friend.Games.Add(app);
+        ((FriendViewModel)BindingContext).Friend.Games.Add(app2);
         Trace.WriteLine("Add Game button clicked.");
     }
 }
